f2fb1a22beffb4ca12d466d9b1128a0deb9a407f
albinosalvadormiguel <albino.salmi@gmail.com>
2024-12-07 19:48:17 +0100

    Listas_Objetos

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
+const movies = ["Lord of the Ring", "Star Wars", "dune"];
+export const animals = [
+  {
+    id: 1,
+    name: "dog",
+    img: "https://nypost.com/wp-content/uploads/sites/2/2022/12/worlds-cutest-dog-comp-1.jpg",
+  },
+  {
+    id: 2,
+    name: "cat",
+    img: "https://img.freepik.com/foto-gratis/lindo-gatito-domestico-sienta-ventana-mirando-fuera-ia-generativa_188544-12519.jpg",
+  },
+  {
+    id: 3,
+    name: "bird",
+    img: "https://media.cnn.com/api/v1/images/stellar/prod/190414090035-01-cassowary.jpg",
+  },
+];
+
+const HTMLmovies = movies.map((movie, id) => {
+  return (
+    <p key={id}>
+      {id} - {movie}
+    </p>
+  );
+});
+
+const HTMLanimals = animals.map((animal) => {
+  return (
+    <div key={animal.id}>
+      <h2>{animal.name}</h2>
+      <img src={animal.img} alt="" width={200} />
+    </div>
+  );
+});
+
 function App() {
   return (
     <div>
-      <h1>Este es el Master</h1>
+      <h1>Renderizado de listas</h1>
+      {HTMLmovies}
+      {HTMLanimals}
     </div>
   );
 }
